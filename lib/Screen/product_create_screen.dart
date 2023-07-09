5474df6f6edda4aa83d01a50095ea4f44d23f46a
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-10 02:07:40 +0600

    make a drop down menu

--- lib/Screen/product_create_screen.dart
+++ lib/Screen/product_create_screen.dart
@@ -28,25 +28,42 @@ class _ProductCreateScreenState extends State<ProductCreateScreen> {
                     decoration: appInputDecoration("Product Name"),
                   ),
                   const SizedBox(height: 20),
-
+                  
                   TextFormField(
                     onChanged: (value) {},
                     decoration: appInputDecoration("Product Code"),
                   ),
                   const SizedBox(height: 20),
-
+                  
                   TextFormField(
                     onChanged: (value) {},
                     decoration: appInputDecoration("Product Image Url"),
                   ),
                   const SizedBox(height: 20),
 
+                  dropDownStyle(
+                    DropdownButton(
+                      value: "",
+                      isExpanded: true,
+                      underline: Container(),
+                      items: [
+                        DropdownMenuItem(child: Text("Select Qty"), value: "",),
+                        DropdownMenuItem(child: Text("1 pcs"), value: "1 pcs",),
+                        DropdownMenuItem(child: Text("2 pcs"), value: "2 pcs",),
+                        DropdownMenuItem(child: Text("3 pcs"), value: "3 pcs",),
+                        DropdownMenuItem(child: Text("4 pcs"), value: "4 pcs",),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  
                   TextFormField(
                     onChanged: (value) {},
                     decoration: appInputDecoration("Unit Price"),
                   ),
                   const SizedBox(height: 20),
-
+                  
                   TextFormField(
                     onChanged: (value) {},
                     decoration: appInputDecoration("Total Price"),
--- lib/Style/style.dart
+++ lib/Style/style.dart
@@ -3,30 +3,42 @@ import 'package:flutter_svg/svg.dart';
 
 const colorRed = Color.fromRGBO(231, 28, 36, 1);
 const colorGreen = Color.fromRGBO(33, 191, 115, 1);
-const colorWhite = Color.fromRGBO(255,255, 255, 1);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1);
 const colorDarkBlue = Color.fromRGBO(231, 28, 36, 1);
 
-InputDecoration appInputDecoration(label){
+InputDecoration appInputDecoration(label) {
   return InputDecoration(
     border: const OutlineInputBorder(),
     focusedBorder: const OutlineInputBorder(
-      borderSide: BorderSide(color: colorGreen, width: 1)
-    ),
+        borderSide: BorderSide(color: colorGreen, width: 1)),
     fillColor: colorWhite,
     filled: true,
     enabledBorder: const OutlineInputBorder(
-        borderSide: BorderSide(color: colorWhite, width: 0)
-    ),
+        borderSide: BorderSide(color: colorWhite, width: 0)),
     contentPadding: const EdgeInsets.fromLTRB(20, 10, 10, 20),
     labelText: label,
   );
 }
 
-SvgPicture backgroundScreen(context){
+SvgPicture backgroundScreen(context) {
   return SvgPicture.asset(
     "assets/images/backgroundImg3.svg",
-    alignment: Alignment.center,fit: BoxFit.fill,
+    alignment: Alignment.center,
+    fit: BoxFit.fill,
     height: MediaQuery.of(context).size.height,
     width: MediaQuery.of(context).size.width,
+  );
+}
+
+DecoratedBox dropDownStyle(child) {
+  return DecoratedBox(
+    decoration: BoxDecoration(
+        color: colorWhite,
+        border: Border.all(color: colorWhite, width: 1),
+        borderRadius: BorderRadius.circular(4)),
+    child: Padding(
+      padding: const EdgeInsets.only(left: 22, right: 22),
+      child: child,
+    ),
   );
 }
